fd3e5db217d6af39f64dbfe536edab63e26fa51c
lucasvitor75 <cebolacutba@gmail.com>
2024-08-19 20:10:14 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>aula</title>
+</head>
+<body>
+    <h1>Teste</h1>
+    <h2>Esse é o meu formulario</h2>
+    <form>
+  <label for="fname">Usuario:</label><br>
+  <input type="text" id="usuario1" name="Usuario"><br>
+  <label for="lname">Senha:</label><br>
+  <input type="text" id="Codsenha" name="Senha"><br>
+  <label for="fname">Email:</label><br>
+  <input type="text" id="Email01" name="Email"><br>
+  <label for="lname">cpf:</label><br>
+  <input type="text" id="Cpf01" name="Cpf">
+  <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
